6f185d978bf663948bbb65fa4bf0dc68b98aa85f
Anagh-Angira <2041001057.anaghangira@gmail.com>
2024-05-11 11:37:14 +0530

    imittialCommit

---
+++ src/in/sp/beans/Student.java
@@ -0,0 +1,31 @@
+package in.sp.beans;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+@Component
+public class Student {
+
+	@Value("Priya")
+	private String name;
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public int getRollno() {
+		return rollno;
+	}
+	public void setRollno(int rollno) {
+		this.rollno = rollno;
+	}
+	@Value("450")
+	private int rollno;
+	
+	public void display()
+	{
+		System.out.println("Name : "+name);
+		System.out.println("Roll no : "+rollno);
+	}
+}
---
+++ src/in/sp/beans/main/Main.java
@@ -0,0 +1,23 @@
+package in.sp.main;
+
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+import in.sp.beans.Student;
+
+public class Main {
+
+	public static void main(String[] args) {
+		@SuppressWarnings("resource")
+		AnnotationConfigApplicationContext context = new 
+				AnnotationConfigApplicationContext();
+		
+		context.register(Student.class);
+		
+		context.refresh();
+		Student std = (Student)context.getBean("student");
+		std.display();
+				
+
+	}
+
+}
